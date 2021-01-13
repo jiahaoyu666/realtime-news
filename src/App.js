8280df277c2fc141b5ef1d09572ea629883d737a
Bruce-Yu2017 <ilywanzi@gmail.com>
2021-01-13 01:18:46 -0800

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import axios from "axios";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, ListGroup } from "react-bootstrap";
+import { Container, ListGroup, Col, Row, Form } from "react-bootstrap";
 
 function App() {
   const [currentData, setData] = useState(null);
+  const [fontSize, setFontSize] = useState(0);
   useEffect(() => {
     const getdata = async () => {
       const { data } = await axios.get(
@@ -16,24 +17,51 @@ function App() {
     };
     getdata();
   }, []);
+  const handleFontSize = (val) => {
+    setFontSize(Number(val));
+  };
   return (
-    <Container>
+    <Container fluid="xl">
       {currentData && (
         <div>
-          <h4 className="text-center mt-2 text-secondary">
-            {currentData.description}
-          </h4>
-          <ListGroup>
-            {currentData.items.map((item, index) => (
-              <ListGroup.Item key={item.id}>
-                {index + 1}.{item.title}(
-                <a target="_blank" rel="noreferrer" href={item.url}>
-                  {new Date(item.date_modified).toLocaleTimeString()}
-                </a>
-                )
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
+          <h2 className="text-center mt-2 text-secondary">
+            新浪全球实时新闻直播
+          </h2>
+          <Form className="pl-5 pr-5">
+            <Form.Group>
+              <Form.Label>调节字体大小</Form.Label>
+              <Form.Control
+                type="range"
+                custom
+                min="1"
+                max="20"
+                step="1"
+                value={fontSize}
+                onChange={(e) => {
+                  handleFontSize(e.target.value);
+                }}
+              />
+            </Form.Group>
+          </Form>
+          <Row className="justify-content-md-center">
+            <Col xs md="auto">
+              <ListGroup>
+                {currentData.items.map((item, index) => (
+                  <ListGroup.Item
+                    key={item.id}
+                    className="main-font-size"
+                    style={{ fontSize: `${20 + fontSize}px` }}
+                  >
+                    {index + 1}.{item.title}(
+                    <a target="_blank" rel="noreferrer" href={item.url}>
+                      {new Date(item.date_modified).toLocaleTimeString()}
+                    </a>
+                    )
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
         </div>
       )}
     </Container>
